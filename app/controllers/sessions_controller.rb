a7189be7a0428734098fae5c31f2f928f970eb8e
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-11-10 17:16:31 +0100

    Include bootstrap to the project

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:session][:name])
-    puts "\n\n\n\n user == #{user[:name]} \n\n\n\n\n"
     unless user.nil?
       session[:user_id] = user.id
       flash[:notice] = 'Logged in successfully.'
