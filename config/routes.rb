146d41d6a6dd074d44f4cd994d98bb59bd6c4dfb
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-11-13 13:33:20 +0100

    Correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users, except: [:new]
+  resources :users, only: [:show, :create]
   resources :articles do
     resources :image_elements
     resources :votes, only: [:create, :destroy]
   end
-  resources :categories
+  resources :categories, only: [:index, :show, :create]
 end
