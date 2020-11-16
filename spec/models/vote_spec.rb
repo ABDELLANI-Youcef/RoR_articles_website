f2a5b47599c467bb4e89ef9dd2c9f7f69797deec
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-11-16 18:48:11 +0100

    Make unit test for vote model

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe Vote, type: :model do
   let(:user) { User.create(name: 'Youcef ABDELLANI') }
   let(:article) { user.articles.create(title: 'First article', text: 'Youcef ABDELLANI is making a test ') }
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:article) }
+  end
+  
   describe 'validations' do
     subject { user.votes.new(article: article) }
     it { should validate_presence_of(:user) }
     it { should validate_presence_of(:article) }
-    it { should validate_uniqueness_of(:user_id).scoped_to(:article_id) }
+    it { should validate_uniqueness_of(:user).scoped_to(:article_id) }
   end
 end
