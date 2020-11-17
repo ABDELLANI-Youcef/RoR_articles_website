77a8ea84fd49003833b1e3861a33ff3ab00e8340
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-11-17 10:10:46 +0100

    add seed of categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,17 +6,6 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.all.includes(:articles)
-    if @categories.nil?
-      a = Category.create(name: 'Technology', priority: 0)
-      a.save
-      a = Category.create(name: 'Books', priority: 1)
-      a.save
-      a = Category.create(name: 'History', priority: 2)
-      a.save
-      a = Category.create(name: 'News', priority: 3)
-      a.save
-      @categories = Category.all.includes(:articles)
-    end
     @best_article = Article.best_article
   end
 
